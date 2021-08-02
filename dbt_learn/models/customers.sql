eac324ce73b1ad04043b41964333a9a4b2009ddf
vdatlvt <v.datlvt@cloudhms.net>
2021-08-02 16:54:40 +0700

    build our first project

--- dbt_learn/models/customers.sql
+++ dbt_learn/models/customers.sql
@@ -1,19 +1,12 @@
-{{ config(materialized = 'table') }}
+{{config(materialized = 'table')}}
 
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from `dbt-tutorial`.jaffle_shop.customers
+    select *
+    from {{ ref('stg_customers') }}
 ),
 orders as (
-    select 
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from `dbt-tutorial`.jaffle_shop.orders
+    select *
+    from {{ ref('stg_orders') }}
 ),
 customer_orders as (
     select 
